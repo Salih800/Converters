0921e6fb6ec0acf4509697013f97a38c2dea63ce
Salih800 <salihcakmak700@gmail.com>
2022-08-09 22:01:37 +0300

    added file_formats

--- images_to_video.py
+++ images_to_video.py
@@ -4,21 +4,28 @@ import glob
 from natsort import natsorted
 import imutils
 
-folder_path = "E:/github-repos/Sorax-Live/"
-folder_name = "saved_images"
+folder_path = "/path/to/images/folder/"
+folder_name = "folder_name"
 
-pics_list = natsorted(glob.glob(os.path.join(folder_path + folder_name + "/*")))
+file_formats = ["jpg", "jpeg", "png"]
+
+pics_list = []
+for file_format in file_formats:
+    format_list = glob.glob(os.path.join(folder_path + folder_name + "/*." + file_format))
+    pics_list.append(format_list)
+
+pics_list = natsorted(pics_list)
 
 set_fps = 15
 fourcc = "mp4v"
 
-video_file_path = folder_name + ".mp4"
-record_width, record_height = (1920, 1080)
+video_file_path = folder_name + "_merged.mp4"
+record_width, record_height = (1280, 720)
 
 out = cv2.VideoWriter(video_file_path, cv2.VideoWriter_fourcc(*fourcc),
                       set_fps, (record_width, record_height))
 
-# print(pics_list)
+print(f"Total {len(pics_list)} picture found")
 for i, pic in enumerate(pics_list):
     print(f"{i+1}/{len(pics_list)}", pic)
     img = cv2.imread(pic)

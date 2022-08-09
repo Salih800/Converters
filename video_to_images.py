033ad47bf62f8020efa9a47dac4734323efe4d48
Salih800 <salihcakmak700@gmail.com>
2022-08-09 22:22:09 +0300

    added jgp-quality and time measure

--- video_to_images.py
+++ video_to_images.py
@@ -1,4 +1,5 @@
 import shutil
+import time
 
 import cv2
 import os
@@ -10,6 +11,7 @@ cap = cv2.VideoCapture(video_path)
 width, height, fps, frame_count = int(cap.get(3)), int(cap.get(4)), cap.get(5), int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
 zfill_count = len(str(frame_count))
+jpg_save_quality = 50
 
 print(f"width: {width}, height: {height}, fps: {fps}, frame_count: {frame_count}")
 
@@ -19,13 +21,14 @@ if os.path.isdir(frames_folder):
 os.makedirs(frames_folder)
 
 frame_number = 0
+start_time = time.time()
 while True:
     ret, img = cap.read()
     if not ret:
-        print(f"Video ended. Total {frame_number} frame extracted.")
+        print(f"Video ended. Total {frame_number} frame extracted in {round(time.time() - start_time, 2)} seconds")
         break
     frame_name = "frame_" + str(frame_number).zfill(zfill_count) + ".jpg"
-    cv2.imwrite(frames_folder + frame_name, img)
+    cv2.imwrite(frames_folder + frame_name, img, [int(cv2.IMWRITE_JPEG_QUALITY), jpg_save_quality])
     frame_number += 1
     print(f"{frame_number}/{frame_count} Frame extracted.")
     
